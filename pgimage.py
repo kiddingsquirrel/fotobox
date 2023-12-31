931dc0325dc1e12a8087efc3a5b7edad8f7b3de1
kiddingsquirrel <jonathan.schmidt.student@gmail.com>
2023-12-31 16:21:31 +0100

    Centered multiline text

--- pgimage.py
+++ pgimage.py
@@ -1,5 +1,5 @@
 import pygame
-
+from PIL import Image, ImageOps, ImageFont, ImageColor, ImageDraw
 class Image:
     def __init__(self,path,location,size):
         self.img = pygame.image.load(path)
@@ -9,3 +9,10 @@ class Image:
         self.img = pygame.image.load(path)
         self.img = pygame.transform.scale(self.img,(self.size[0],self.size[1]))
         print("Updated image to path:" + path)
+    def text_to_image(self,text,font="Oswald/Oswald-VariableFont_wght.ttf",fontsize=50):
+        self.font= ImageFont.truetype(font,fontsize)
+        self.img=Image.new(mode="RGBA",size=self.size)
+        self.draw = ImageDraw.Draw(self.img)
+        self.draw.text(())
+
+        pass
--- pil_draw_text_on_image.py
+++ pil_draw_text_on_image.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageOps, ImageFont, ImageColor, ImageDraw
 # Image size 4x1 Montage 600x135 pixel
 # Image size 2x2 Monate 1725x129 pixel
 
+    
 font = ImageFont.truetype("Oswald/Oswald-VariableFont_wght.ttf",50)
 
 thumb_4x1 = Image.new(mode="RGBA",size=(600,135),color="gray")
@@ -13,15 +14,19 @@ draw_4x1.text(((thumb_4x1.width-font_width)/2,thumb_4x1.height/2-1.1*font_heigth
 text="10.10.2023"
 font_width,font_heigth =font.getsize(text)
 draw_4x1.text(((thumb_4x1.width-font_width)/2,thumb_4x1.height/2),text,font=font,fill="black")
-thumb_4x1.show()
+#thumb_4x1.show()
 
 
-font = ImageFont.truetype("Oswald/Oswald-VariableFont_wght.ttf",80)
+font = ImageFont.truetype("Oswald/Oswald-VariableFont_wght.ttf",50)
 thumb_2x2 = Image.new(mode="RGBA",size=(1725,129),color="gray")
 draw_2x2=ImageDraw.Draw(thumb_2x2)
 font
 text="Eillen & Ronny 11.11.2023"
 font_width,font_heigth =font.getsize(text)
 draw_2x2.text(((thumb_2x2.width-font_width)/2,(thumb_2x2.height-font_heigth)/2),text,font=font,fill="black")
-thumb_2x2.show()
+#thumb_2x2.show()
 
+test = Image.new(mode="RGBA",size=(600,129),color="gray")
+draw=ImageDraw.Draw(test)
+draw.multiline_text((test.width/2,test.height/2),"Guten Morgen",anchor="mm",align="center",font=font)
+test.show()
