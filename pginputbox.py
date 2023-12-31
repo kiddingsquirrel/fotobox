c70f0a3db6e44ff49cde7d55fe768344feb7f661
kiddingsquirrel <jonathan.schmidt.student@gmail.com>
2023-12-31 08:43:13 +0100

    remove global variables for class

--- pginputbox.py
+++ pginputbox.py
@@ -2,17 +2,21 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((640,480))
-COLOR_INACTIVE=pygame.Color('lightskyblue3')
-COLOR_ACTIVE=pygame.Color('dodgerblue2')
-FONT= pygame.font.Font(None,32)
+
 
 
 class InputBox:
-    def __init__(self,location,size,text='Input Text'):
+    def __init__(self,location,size,text='Input Text',
+                 COLOR_INACTIVE=pygame.Color('lightskyblue3'),
+                 COLOR_ACTIVE=pygame.Color('dodgerblue2'),
+                 FONT= pygame.font.Font(None,32)):
         self.rect=pygame.Rect(location[0],location[1],location[0]+size[0],location[1]+size[1])
-        self.color=COLOR_INACTIVE
+        self.COLOR_INACTIVE=COLOR_INACTIVE
+        self.COLOR_ACTIVE=COLOR_ACTIVE
+        self.FONT=FONT
+        self.color=self.COLOR_INACTIVE
         self.text=text
-        self.txt_surface = FONT.render(text,True,self.color)
+        self.txt_surface = self.FONT.render(text,True,self.color)
         self.active=False
     def handle_event(self,event):
         if event.type==pygame.MOUSEBUTTONDOWN:
@@ -23,7 +27,7 @@ class InputBox:
             else:
                 self.active=False
             # Change the current color of the input box
-            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+            self.color = self.COLOR_ACTIVE if self.active else self.COLOR_INACTIVE
         if event.type==pygame.KEYDOWN:
             if self.active:
                 if event.key == pygame.K_RETURN:
@@ -34,7 +38,7 @@ class InputBox:
                 else:
                     self.text += event.unicode
                 # Re-render the text.
-                self.txt_surface = FONT.render(self.text,True,self.color)
+                self.txt_surface = self.FONT.render(self.text,True,self.color)
     def draw(self,screen):
         # Blit the text
         screen.blit(self.txt_surface,(self.rect.x+5,self.rect.y+5))
