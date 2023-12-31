43aee53e738f571b826e80cae59583589dce97ae
kiddingsquirrel <jonathan.schmidt.student@gmail.com>
2023-12-31 08:55:43 +0100

    Clean class not working on its own

--- pginputbox.py
+++ pginputbox.py
@@ -1,6 +1,6 @@
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((640,480))
+#pygame.init()
+#screen = pygame.display.set_mode((640,480))
 class InputBox:
     def __init__(self,location,size,text='Input Text',
                  COLOR_INACTIVE=pygame.Color('lightskyblue3'),
@@ -41,25 +41,25 @@ class InputBox:
         # Blit the rect
         pygame.draw.rect(screen,self.color,self.rect,2)
 
-def main():
-    print("main")
-    clock = pygame.time.Clock()
-    input_box1=InputBox((0,0),(200,30),"Zeile 1 - Press Enter to start")
-    input_box2=InputBox((0,40),(200,30),"Zeile 2 - Press Enter to starts")
-    input_boxes=[input_box1,input_box2]
-    done= False
-    while not done:
-        print("loop")
-        for event in pygame.event.get():
-            if event.type== pygame.QUIT:
-                done=True
-            for box in input_boxes:
-                box.handle_event(event)
-        screen.fill((30,30,30))
-        for box in input_boxes:
-            box.draw(screen)        
-        pygame.display.flip()
-        clock.tick(30)
-print("start")
-main()
-pygame.quit()
+#def main():
+#    print("main")
+#    clock = pygame.time.Clock()
+#    input_box1=InputBox((0,0),(200,30),"Zeile 1 - Press Enter to start")
+#   input_box2=InputBox((0,40),(200,30),"Zeile 2 - Press Enter to starts")
+#    input_boxes=[input_box1,input_box2]
+#    done= False
+#    while not done:
+#        #print("loop")
+#        for event in pygame.event.get():
+#            if event.type== pygame.QUIT:
+#                done=True
+#            for box in input_boxes:
+#                box.handle_event(event)
+#        screen.fill((30,30,30))
+#        for box in input_boxes:
+#            box.draw(screen)        
+#        pygame.display.flip()
+#        clock.tick(30)
+#print("start")
+#main()
+#pygame.quit()
